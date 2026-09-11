e8f4680db240657b3f26d2fc303f46c4bd7290a3
solineleveaux <soline.leveaux@gmail.com>
2026-09-11 16:13:34 +0200

    Implémentation constructeur Client

---
+++ Client.java
@@ -0,0 +1,49 @@
+public class Client {
+
+    protected String adresseLivraison ;
+    protected String numeroTelephone ;
+    protected String moyenPaiement ;
+
+    public Client(String addresse, String numero, String paiement)
+    {
+        this.adresseLivraison = addresse ;
+        this.numeroTelephone = numero ;
+        this.moyenPaiement = paiement ;
+    }
+
+    private void passerCommande (Commande panier)
+    {
+
+    }
+
+    public String getAdresseLivraison ()
+    {
+
+    }
+
+    public String getnumeroTelephone ()
+    {
+
+    }
+
+    public String getMoyenPaiement ()
+    {
+
+    }
+
+    protected void setAdresseLivraison (String nouvelle_adresse)
+    {
+
+    }
+
+    protected void setNumeroTelephone (String nouveau_numero)
+    {
+
+    }
+
+    protected void setMoyenPaiement (String nouveau_paiement)
+    {
+
+    }
+
+}
